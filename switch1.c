105100470bdc38d29c17fd1f42399f0e050f0278
srujan gowda <srujan.gowda@s.amity.edu>
2025-03-25 12:20:39 +0530

    Update switch1.c

--- switch1.c
+++ switch1.c
@@ -12,3 +12,19 @@ int main() {
         case 1:
             printf("B tech in CSE\n");
             break;
+ case 2:
+            printf("B.Tech in AI ML\n");
+            break;
+        case 3:
+            printf("B.Tech in ECE\n");
+            break;
+case 4:
+            printf("B.Tech in civil\n");
+            break;
+default:
+            printf("Invalid input! Please enter a number between 1 and 7.\n");
+            break;
+    }
+
+    return 0;
+}
